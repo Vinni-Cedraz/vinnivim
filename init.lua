6c8b09e27fae6da3cf441db595920efdf84192ee
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2022-11-13 22:12:19 -0300

    formatted with stylua

--- init.lua
+++ init.lua
@@ -18,49 +18,49 @@ require("user.indentline")
 require("user.alpha")
 require("user.lsp")
 require("user.dap")
-require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = true,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-     accept = "<right>",
-     next = "<S-tab>",
-     dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  require("copilot_cmp").setup{
-  method = "getCompletionsCycling",
-  formatters = {
-    label = require("copilot_cmp.format").format_label_text,
-    insert_text = require("copilot_cmp.format").format_insert_text,
-    preview = require("copilot_cmp.format").deindent,
- },
+require("copilot").setup({
+	panel = {
+		enabled = true,
+		auto_refresh = true,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<right>",
+			next = "<S-tab>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	require("copilot_cmp").setup({
+		method = "getCompletionsCycling",
+		formatters = {
+			label = require("copilot_cmp.format").format_label_text,
+			insert_text = require("copilot_cmp.format").format_insert_text,
+			preview = require("copilot_cmp.format").deindent,
+		},
 
-  copilot_node_command = 'node', -- Node version must be < 18
-  plugin_manager_path = vim.fn.stdpath("data") .. "/site/pack/packer",
-  server_opts_overrides = {},
-}
+		copilot_node_command = "node", -- Node version must be < 18
+		plugin_manager_path = vim.fn.stdpath("data") .. "/site/pack/packer",
+		server_opts_overrides = {},
+	}),
 })
